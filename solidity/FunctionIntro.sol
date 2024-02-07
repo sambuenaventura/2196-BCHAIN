b549ebd4a4d7ed7c65675e422195eb432cdc5437
Sam Buenaventura <94029953+sambuenaventura@users.noreply.github.com>
2024-02-07 21:27:56 +0800

    Delete solidity directory

--- solidity/FunctionIntro.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.24;
-
-contract FunctionIntro {
-    function add(uint x, uint y) external pure returns (uint) {
-        return (x+y);
-    }
-}
